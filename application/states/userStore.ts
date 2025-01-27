e70f9b6a845ab110572f182742159d5f5e06a9e2
m01310g <m01310g@gmail.com>
2025-01-27 14:36:21 +0900

    Merge branch 'develop' of https://github.com/FRONT-END-BOOTCAMP-PLUS-3/myking into user

--- application/states/userStore.ts
+++ application/states/userStore.ts
@@ -2,20 +2,25 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface UserState {
+    isLoggedIn: boolean;
     kakaoId: string | "";
     name: string | "";
     nickname: string;
-    setUser: (kakaoId: string, name: string, nickname: string) => void;
+    setUser: (kakaoId: string, name: string, nickname: string) => void; // 사용자 정보 설정
+    resetUser: () => void; // 상태 초기화(로그아웃)
 }
 
 export const useUserStore = create<UserState>()(
     persist(
         (set) => ({
+            isLoggedIn: false,
             kakaoId: "",
             name: "",
             nickname: "",
             setUser: (kakaoId, name, nickname) =>
-                set({ kakaoId, name, nickname }),
+                set({ isLoggedIn: true, kakaoId, name, nickname }),
+            resetUser: () =>
+                set({ isLoggedIn: false, kakaoId: "", name: "", nickname: "" }),
         }),
         {
             name: "user-storage",
