3dc64b7be9611b0b13ae73b03a41d4e577f5d488
m01310g <m01310g@gmail.com>
2025-01-28 21:18:44 +0900

    feat: 세션 만료 시 사용자에게 alert하고 /auth로 리다이렉트 - #16

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -3,6 +3,7 @@
 import { useUserStore } from "@/application/states/userStore";
 import { restoreSession } from "@/components/user/AuthUtils";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function AuthProvider({
@@ -12,11 +13,27 @@ export default function AuthProvider({
 }) {
     const supabase = createClientComponentClient();
     const { setUser, resetUser, hasHydrated } = useUserStore((state) => state);
+    const router = useRouter();
 
     useEffect(() => {
         if (!hasHydrated) return;
 
         restoreSession(setUser, resetUser);
+
+        // 실시간 세션 상태 감시
+        const { data: subscription } = supabase.auth.onAuthStateChange(
+            (event, session) => {
+                if (event === "SIGNED_OUT" || !session) {
+                    alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+                    resetUser();
+                    router.push("/auth");
+                }
+            }
+        );
+
+        return () => {
+            subscription?.subscription.unsubscribe();
+        };
     }, [supabase, setUser, resetUser]);
 
     return <>{children}</>;
