da52a009437803ab6eff5cd484508d01994f464e
gayeongogo <rkdus1357@gmail.com>
2025-02-11 11:01:28 +0900

    feat: 파티 생성 유저 프로필사진, 닉네임 구현

--- domain/repositories/UserRepository.ts
+++ domain/repositories/UserRepository.ts
@@ -3,8 +3,9 @@ import { User } from "../entities/User";
 export interface UserRepository {
     create(user: User): Promise<void>;
     findByNickname(nickname: string): Promise<User | null>;
-    findById(kakaoId: string): Promise<User | null>;
+    findById(kakaoId: string): Promise<User>;
     getUsers(): Promise<User[]>;
     deleteUser(userId: string): Promise<void>;
     updateNickname(kakaoId: string, newNickname: string): Promise<User | null>;
+    getUserByUuid(userId: string): Promise<User>;
 }
