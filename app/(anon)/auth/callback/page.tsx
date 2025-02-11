765eb3ce9a911774b19bc6cbc946827bef40da05
m01310g <m01310g@gmail.com>
2025-02-11 14:51:59 +0900

    feat: 로딩 스피너 추가

--- app/(anon)/auth/callback/page.tsx
+++ app/(anon)/auth/callback/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useUserStore } from "@/application/states/userStore";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import ProtectedRoute from "@/components/user/ProtectedRoutes";
+import LoadingSpinner from "@/components/loadingSpinner/loadingSpineer";
 
 export default function AuthCallback() {
     const { setUser, resetUser } = useUserStore((state) => state);
     const router = useRouter();
     const supabase = createClientComponentClient();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const handleAuthCallback = async () => {
@@ -57,6 +59,8 @@ export default function AuthCallback() {
                 console.error("로그인 처리 중 오류 발생: ", error);
                 alert("오류가 발생했습니다. 다시 시도해주세요.");
                 router.push("/auth");
+            } finally {
+                setIsLoading(false);
             }
         };
 
@@ -65,7 +69,7 @@ export default function AuthCallback() {
 
     return (
         <ProtectedRoute>
-            <div>로그인 처리 중...</div>
+            {isLoading ? <LoadingSpinner /> : <div>로그인 처리중...</div>}
         </ProtectedRoute>
     );
 }
